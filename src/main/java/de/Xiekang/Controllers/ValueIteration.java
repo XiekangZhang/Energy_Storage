e45ed50f88a6d7728597dd0de864472a45647739
x_zhan08 <XiekangZhang@users.noreply.github.com>
2018-07-05 20:21:51 +0200

    fertig mit programmieren

--- src/main/java/de/Xiekang/Controllers/ValueIteration.java
+++ src/main/java/de/Xiekang/Controllers/ValueIteration.java
@@ -22,9 +22,10 @@ public class ValueIteration extends TreeStructure {
     private DefaultMutableTreeNode defaultMutableTreeNode;
     private TreeStructure treeStructure;
     private Node<State<Double, StateOfMarket<Integer, Integer, Double>>> child;
-    private List<State<Double, String>> path = new ArrayList<>();
+    private State<Double, StateOfMarket<Integer, Integer, Double>>[][] path;
+    private List<State<Double, StateOfMarket<Integer, Integer, Double>>> paths = new ArrayList<>();
     private List<Double> values = new ArrayList<>();
-    private double value= 0;
+    private double value = 0;
 
     public DefaultMutableTreeNode init() {
         treeStructure = new TreeStructure();
@@ -50,45 +51,92 @@ public class ValueIteration extends TreeStructure {
     public ValueIteration() {
         init();
         child = treeStructure.getRoot1();
+        path = new State[defaultMutableTreeNode.getLeafCount()][defaultMutableTreeNode.getDepth() + 1];
 
+        for (int i = 0; i < path.length; i++) {
+            for (int j = 0; j < path[i].length; j++) {
+                path[i][j] = new State<>();
+            }
+        }
         findTheDepthChildren(child);
-        this.toString();
+        createPathArray();
+//        findTheOptimalWithTime();
+        findTheOptimal(1.7);
     }
 
     public void findTheDepthChildren(Node<State<Double, StateOfMarket<Integer, Integer, Double>>> child) {
-        if (!child.getChildren().isEmpty() && child.getChildren().get(0).getChildren().isEmpty()) {
-            calculateValueIteration(child);
+        if (child.getChildren().isEmpty()) {
+            iterateChildren(child);
+//            System.out.println(child.getData().toString());
         } else {
             child.getChildren().forEach(c1 -> findTheDepthChildren(c1));
         }
     }
 
-    public void calculateValueIteration(Node<State<Double, StateOfMarket<Integer, Integer, Double>>> child) {
-
-        while (child.getParent() != null) {
-            for (int i = 0; i < child.getChildren().size(); i++) {
-                path.add(new State<>(value, child.getChildren().get(i).getData().toString()));
-                values.add((child.getData().getV1() - child.getChildren().get(i).getData().getV1()) *
-                        child.getData().getV2().getExpectation() *
-                        child.getData().getV2().getPrice());
-            }
-            value = Collections.max(values);
-            values.clear();
-
+    public void iterateChildren(Node<State<Double, StateOfMarket<Integer, Integer, Double>>> child) {
+//        System.out.println(child.getData().toString());
+        paths.add(child.getData());
+        if (child.getParent() != null) {
             child = child.getParent();
-            calculateValueIteration(child);
+            iterateChildren(child);
         }
     }
 
-
-    public void printTest(Node<State<Double, StateOfMarket<Integer, Integer, Double>>> child) {
-        System.out.println(child.getData().toString());
+    public void createPathArray(){
+        for (int row = 0; row < path.length; row++) {
+            for (int limit = 0; limit < path[row].length; limit++) {
+                path[row][limit] = paths.get((row) * 4 + limit);
+            }
+        }
+        calculateValueIteration();
     }
+
+    public void calculateValueIteration() {
+        for (int i = 0; i < path.length; i++) {
+            value = 0;
+            for (int j = 1; j < path[i].length; j++) {
+                value += (path[i][j].getV1() - path[i][j-1].getV1()) * path[i][j].getV2().getPrice() * path[i][j].getV2().getExpectation();
+            }
+            values.add(value);
+        }
+    }
+
+    public void findTheOptimalWithTime() {
+        value = Collections.max(values);
+        int index = values.indexOf(value);
+        for (int j = 0; j < path[index].length; j++) {
+            System.out.print(path[index][j] + " <- ");
+        }
+        System.out.print(value);
+    }
+
+    public void findTheOptimal(double profit) {
+        List<Double> v1 = new ArrayList<>();
+        for (double v : values) {
+            v1.add(Math.abs((profit - v) / v));
+        }
+
+        value = Collections.min(v1);
+        int index = v1.indexOf(value);
+        for (int j = 0; j < path[index].length; j++) {
+            System.out.print(path[index][j] + " <- ");
+        }
+        System.out.print(profit);
+    }
+
 
     @Override
     public String toString() {
-        path.forEach(state -> System.out.println(state.toString()));
-        System.out.println(path.size());
+        for (int i = 0; i < path.length; i++) {
+            System.out.print(i + " ");
+            for (int j= 0; j < path[i].length; j++) {
+                System.out.print(path[i][j] + " ");
+            }
+            System.out.println();
+        }
+        values.forEach(v -> System.out.println(v));
+//        paths.forEach(stateNode -> System.out.println(stateNode));
+//        System.out.println(paths.size());
         return "calculating...";
     }
 }
